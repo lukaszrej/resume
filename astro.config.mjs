461b20114d748f38058154ee857d3bae24a4a0ef
Łukasz Rej <lukaszrej@ukaszs-MacBook-Pro.local>
2026-01-10 18:13:26 +0100

    Fix astro config file for github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import react from "@astrojs/react";
 
 export default defineConfig({
   site: "https://lukaszrej.github.io",
-  base: 'https://github.com/lukaszrej/resume',
+  base: "/resume",
   integrations: [react()],
   vite: {
     plugins: [tailwindcss()],
