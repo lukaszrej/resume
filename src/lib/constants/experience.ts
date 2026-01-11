1ac3f8438fa4399e9f8028b1294433f116e5b38d
Łukasz Rej <lukaszrej@ukaszs-MacBook-Pro.local>
2026-01-11 14:14:21 +0100

    Add proper expierence

--- src/lib/constants/experience.ts
+++ src/lib/constants/experience.ts
@@ -7,21 +7,115 @@ export const EXPERIENCES: Experience[] = [
       {
         title: "Frontend React.js Developer",
         year: "03.2023 - present",
-        description: `
-Developed a React.js loyalty app for installers from the ground up..
-            `,
+        description:
+          "Developed a React.js loyalty app for installers from the ground up.",
         skills: [
           "TypeScript",
-          "React",
-          "Tailwind CSS",
-          "Node.js",
-          "PostgreSQL",
+          "React.js",
+          "Material UI",
+          "Emotion",
+          "Tanstack Query",
+          "Tanstack Router",
+          "React Hook Form",
           "Vite",
+          "React Testing Library",
+          "Vitest",
         ],
       },
     ],
   },
-
+  {
+    company: "SNI Group",
+    positions: [
+      {
+        title: "React.js Developer with German",
+        year: "10.2022 - 02.2023",
+        description:
+          "Contributed to a project for a major German public institution.",
+        skills: [
+          "TypeScript",
+          "React.js",
+          "GraphQL",
+          "Apollo Client",
+          "AntDesign",
+          "React Testing Library",
+        ],
+      },
+    ],
+  },
+  {
+    company: "Qualent Software",
+    positions: [
+      {
+        title: "React.js Developer",
+        year: "03.2021 - 10.2022",
+        description:
+          "Developed a React.js application for renting construction machinery and participated in a Next.js CMS migration project (from Prismic to Strapi).",
+        skills: [
+          "React.js",
+          "JavaScript (ES6+)",
+          "Redux",
+          "GraphQL",
+          "Apollo Client",
+          "Styled Components",
+          "React Query",
+          "React Router",
+          "Material UI",
+          "Formik",
+          "REST API",
+          "Next.js",
+          "Strapi",
+          "Emotion",
+          "Prismic",
+          "AWS",
+        ],
+      },
+    ],
+  },
+  {
+    company: "Agora S.A.",
+    positions: [
+      {
+        title: "React.js Frontend Developer",
+        year: "08.2020 - 03.2021",
+        description:
+          "Led the development of a React.js-based advertisement reservation app, developed and maintained ad display logic, including implementing new ad templates.",
+        skills: [
+          "TypeScript",
+          "React.js",
+          "Redux",
+          "Styled Components",
+          "React Router",
+          "Material UI",
+          "Formik",
+          "REST API",
+          "HTML5",
+          "CSS3",
+          "JavaScript",
+        ],
+      },
+    ],
+  },
+  {
+    company: "Miinto Group",
+    positions: [
+      {
+        title: "Junior Frontend Developer",
+        year: "12.2019 - 05.2020",
+        description:
+          "Contributed to the development of the Miinto e-commerce platform, helped build an internal React.js components library.",
+        skills: [
+          "React.js",
+          "JavaScript",
+          "SCSS",
+          "HTML5",
+          "CSS3",
+          "jQuery",
+          "Bootstrap",
+        ],
+      },
+    ],
+  },
   {
     company: "Education",
     positions: [
