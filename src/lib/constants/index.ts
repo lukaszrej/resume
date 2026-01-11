ef82e37927a1d60c390711eda1f93ea48b2cc583
Łukasz Rej <lukaszrej@ukaszs-MacBook-Pro.local>
2026-01-11 11:32:37 +0100

    Fix social links

--- src/lib/constants/index.ts
+++ src/lib/constants/index.ts
@@ -1,23 +1,15 @@
 import LinkedinIcon from "@/components/ui/icons/linkedin.astro";
-import XIcon from "@/components/ui/icons/x.astro";
 import GithubIcon from "@/components/ui/icons/github.astro";
-
-export const NAV_LINKS = [
-  {
-    label: "Home",
-    href: "/",
-  },
-] as const;
 
 export const SOCIAL_LINKS = [
   {
     name: "Github",
     icon: GithubIcon,
-    url: "https://github.com/lukasz-rej",
+    url: "https://github.com/lukaszrej",
   },
   {
     name: "Linkedin",
     icon: LinkedinIcon,
-    url: "https://www.linkedin.com/in/lukaszrej/",
+    url: "https://www.linkedin.com/in/lukasz-rej/",
   },
 ];
